666c76c58af95b8362b0a872e02b2dc197cd12c6
Piyush Dangre <piyushdangre11@gmail.com>
2020-06-21 13:36:25 +0530

    Adding HIBERNATE CRUD Operations

--- src/com/app/hibernate/CreateStudentDemo.java
+++ src/com/app/hibernate/CreateStudentDemo.java
@@ -1,5 +1,7 @@
 package com.app.hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -50,6 +52,70 @@ public class CreateStudentDemo {
 			session.getTransaction().commit();
 					
 			System.out.println("\nStudent object Read - "+st1);
+			
+			/**
+			 * ** -- QUERY THE TABLE
+			 * We use HQL (Hibernate Query language)
+			 * Inside the HQL, we use Entity class names instead of table names as in SQL.
+			 * Inside the HQL, we use Entity class attribute names instead of column names as in SQL.
+			 * We are not using SELECT * in the query as that is understood.
+			 */
+			
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			
+			@SuppressWarnings("unchecked")
+			List<Student> result = session.createQuery("from Student s where s.firstName='Piyush'")
+			.getResultList();
+			
+			session.getTransaction().commit();
+			
+			System.out.println("\nFetched results");
+			for(Student s : result) {
+				System.out.println(s);
+			}
+			
+			/**
+			 * Updating objects.
+			 * No need to call the .update or .save method explicitly. As the Student 
+			 * Object is a persistent object. So just calling a setter on it will update 
+			 * the DB.
+			 */
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			
+			Student s = session.get(Student.class, st.getId());
+			
+			s.setFirstName("Ameer"); // This will automatically update in DB. As s is a persistent object. Magic of hibernate.
+			
+			session.getTransaction().commit();
+			
+			/**
+			 * --- UPDATE QUERY
+			 */
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			
+			session.createQuery("update Student set lastName='Piku'").executeUpdate();
+			session.getTransaction().commit();
+			
+			/**
+			 * Delete an object
+			 */
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			session.delete(st);
+			session.getTransaction().commit();
+			
+			/**
+			 * Deleting with query
+			 */
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			session.createQuery("delete from Student where id=2")
+				   .executeUpdate();
+			session.getTransaction().commit();
+
 			
 		}finally {
 			
